60ba96984560fb847d5cb2f3db859ce07c93a03c
Mr.V <67038533+vardhanngg@users.noreply.github.com>
2025-08-22 12:12:57 +0530

    Create config.js

---
+++ frontend/config.js
@@ -0,0 +1,4 @@
+// Same-origin API base (Express serves both frontend & API)
+export const API_BASE = "";
+export const SEARCH_ENDPOINT = `${API_BASE}/api/search`;
+export const SONG_BY_MOOD_ENDPOINT = `${API_BASE}/api/songByMood`;
